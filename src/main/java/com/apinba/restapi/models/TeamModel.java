d9a5c2c05785ffc77608034d56bc70646206fec3
Fausto Moya <faustomoya99@gmail.com>
2024-11-09 13:00:31 +0100

    fix: wildcard imports

--- src/main/java/com/apinba/restapi/models/TeamModel.java
+++ src/main/java/com/apinba/restapi/models/TeamModel.java
@@ -1,78 +1,73 @@
 package com.apinba.restapi.models;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "team")
 public class TeamModel {
-    @Id
-    @Column
-    private UUID id = UUID.randomUUID();
+  @Id @Column private UUID id = UUID.randomUUID();
 
-    @Column
-    private String abbreviation;
-    @Column
-    private String city;
-    @Column
-    private String conference;
-    @Column
-    private String division;
-    @Column
-    private String full_name;
-    @Column
-    private String name;
+  @Column private String abbreviation;
+  @Column private String city;
+  @Column private String conference;
+  @Column private String division;
+  @Column private String full_name;
+  @Column private String name;
 
-    public String getAbbreviation() {
-        return abbreviation;
-    }
+  public String getAbbreviation() {
+    return abbreviation;
+  }
 
-    public void setAbbreviation(String abbreviation) {
-        this.abbreviation = abbreviation;
-    }
+  public void setAbbreviation(String abbreviation) {
+    this.abbreviation = abbreviation;
+  }
 
-    public String getCity() {
-        return city;
-    }
+  public String getCity() {
+    return city;
+  }
 
-    public void setCity(String city) {
-        this.city = city;
-    }
+  public void setCity(String city) {
+    this.city = city;
+  }
 
-    public String getConference() {
-        return conference;
-    }
+  public String getConference() {
+    return conference;
+  }
 
-    public void setConference(String conference) {
-        this.conference = conference;
-    }
+  public void setConference(String conference) {
+    this.conference = conference;
+  }
 
-    public String getDivision() {
-        return division;
-    }
+  public String getDivision() {
+    return division;
+  }
 
-    public void setDivision(String division) {
-        this.division = division;
-    }
+  public void setDivision(String division) {
+    this.division = division;
+  }
 
-    public String getFull_name() {
-        return full_name;
-    }
+  public String getFull_name() {
+    return full_name;
+  }
 
-    public void setFull_name(String full_name) {
-        this.full_name = full_name;
-    }
+  public void setFull_name(String full_name) {
+    this.full_name = full_name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public UUID getId() {
-        return id;
-    }
+  public UUID getId() {
+    return id;
+  }
 }
