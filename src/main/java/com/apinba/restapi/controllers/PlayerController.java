f9e0dea2c8693b6c31b641f8a2937a32823c641d
Fausto <moya04.ramiro@gmail.com>
2025-03-05 16:35:00 -0300

    permitir cors para vercel

--- src/main/java/com/apinba/restapi/controllers/PlayerController.java
+++ src/main/java/com/apinba/restapi/controllers/PlayerController.java
@@ -9,7 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://mr-nba.vercel.app")
 @RestController
 @RequestMapping("/players")
 public class PlayerController {
--- src/main/java/com/apinba/restapi/controllers/TeamController.java
+++ src/main/java/com/apinba/restapi/controllers/TeamController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://mr-nba.vercel.app")
 @RestController
 @RequestMapping("/teams")
 public class TeamController {
